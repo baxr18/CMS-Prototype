15c0be7423ff664dc736cb36a9dc5f5ee4ddbd13
baxr18 <77725000+baxr18@users.noreply.github.com>
2021-03-04 14:02:38 +0000

    Update and rename index.md to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>CMS Tutorial</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <div class="container">
+            <a href="index.php" id="logo">CMS</a>
+
+        <ol>
+            <li><a href="article.php?id=1">Article Title></a></li>
+        </ol>
+    </div>
+    </body>
+</html>
